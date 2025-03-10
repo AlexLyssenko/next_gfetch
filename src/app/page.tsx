5eb4593df9e408a3f70094dba009a8961a8539dd
alyssenko <Alexandr.Lyssenko@dataart.com>
2025-03-10 18:41:42 +0500

    failed test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read2
+            Read5
           </a>
         </div>
       </main>
